14f540dad7ae1fa3b18f3e0d3d6df4a2becd041f
S. Marie Kav <siobhan.harley@gmail.com>
2017-02-02 03:00:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect (array)
+if block_given?
+i = 0
+new_arr = []
+while i < array.length
+  new_result = yield(array[i])
+  new_arr.push(new_result)
+  i += 1
+end
+return new_arr
+else
+  "Hey, there is no block!"
+end
+end
